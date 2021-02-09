c1bbaee0bdb1ae0e2a76eef18de325b486b40c93
Ricky Muli <mrickymuliawan@gmail.com>
2021-02-09 15:13:15 +0700

    tes

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,11 +3,6 @@
  */
 
 import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   expect(1).toBe(1)
